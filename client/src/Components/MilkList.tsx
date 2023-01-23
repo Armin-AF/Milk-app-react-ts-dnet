8fb3ab34f6399441de18a2da985b26489a7d8650
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-01-23 12:26:49 +0100

    Adds Search Filter

--- client/src/Components/MilkList.tsx
+++ client/src/Components/MilkList.tsx
@@ -4,6 +4,7 @@ import milkImage from './milk.png';
 function MilkList() {
     const [milkData, setMilkData] = useState([]);
     const [searchValue, setSearchValue] = useState('');
+    const [milkType, setMilkType] = useState('');
 
     useEffect(() => {
         fetch('https://localhost:7237/Milk')
@@ -15,9 +16,23 @@ function MilkList() {
         setSearchValue(event.target.value);
     };
 
-    const filteredData = milkData.filter(milk => {
-        return milk.name.toLowerCase().includes(searchValue.toLowerCase());
-    });
+    const handleMilkTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setMilkType(event.target.value);
+    };
+
+    let filteredData = milkData;
+
+    if (searchValue !== '') {
+        filteredData = filteredData.filter(milk => {
+            return milk.name.toLowerCase().includes(searchValue.toLowerCase());
+        });
+    }
+
+    if (milkType !== '') {
+        filteredData = filteredData.filter(milk => {
+            return milk.type === milkType;
+        });
+    }
 
     return (
         <div>
@@ -28,16 +43,37 @@ function MilkList() {
                 onChange={handleSearch}
                 className="bg-gray-200 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
             />
+            <select
+                value={milkType}
+                onChange={handleMilkTypeChange}
+                className="bg-gray-200 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+            >
+                <option value="">All Types</option>
+                <option value="Whole milk">Whole Milk</option>
+                <option value="Oat milk">Oat Milk</option>
+                <option value="Pea milk">Pea Milk</option>
+                <option value="Almond milk">Almond Milk</option>
+                <option value="Rice milk">Rice Milk</option>
+                <option value="Coconut milk">Coconut Milk</option>
+                <option value="Soy milk">Soy Milk</option>
+                <option value="Walnut milk">Walnut Milk</option>
+                <option value="Macadamia milk">Macadamia Milk</option>
+                <option value="Hemp milk">Hemp Milk</option>
+                <option value="Cashew milk">Cashew Milk</option>
+            </select>
+            <h1 className="text-1xl font-bold">{milkData.length} Products</h1>
             <div className="flex flex-wrap">
-                {filteredData.map(milk => (
-                    <div key={milk.id} className="w-1/3 p-4">
-                        <div className="bg-white rounded-lg shadow-md">
-                            <img src={milkImage} alt="Milk" className="w-full" />
-                            <div className="p-4">
-                                <h2 className="text-lg font-medium">{milk.name}</h2>
-                                <p className="text-gray-600">{milk.type}</p>
-                                <p className="text-gray-600">{milk.storage} liters</p>
-                            </div>
+                {filteredData.map((milk, index) => (
+                    <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg">
+                        <img src={milkImage} alt="Milk" className="w-full" />
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">{milk.name}</div>
+                            <p className="text-gray-700 text-base">
+                                Type: {milk.type}
+                            </p>
+                            <p className="text-gray-700 text-base">
+                                Storage: {milk.storage} liter
+                            </p>
                         </div>
                     </div>
                 ))}
